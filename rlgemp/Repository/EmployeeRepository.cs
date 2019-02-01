e2c8fe1306e96c8e906872f2bdf40bd922f3ec95
Cogassment <46102794+Cogassment@users.noreply.github.com>
2019-02-01 16:04:41 +0530

    Add files via upload

---
+++ rlgemp/Repository/EmployeeRepository.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Helpers;
+using rlgemp.Models;
+
+namespace rlgemp.Repository
+{
+    public class EmployeeRepository
+    {
+        public string InsertEmployeeDetails(EmployeeDetails employeeDetail)
+        {
+            string viewMessage = string.Empty;
+            string connectionstring = ConfigurationManager.ConnectionStrings["Constring"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connectionstring);
+            string query = "insert INTO empdetails VALUES(@id,@role,@associatename,@projectname,@managermane)";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.Add("@id", SqlDbType.VarChar).Value = employeeDetail.Associateid;
+                command.Parameters.Add("@role", SqlDbType.VarChar).Value = employeeDetail.Role;
+                command.Parameters.Add("@associatename", SqlDbType.VarChar).Value = employeeDetail.Associatename;
+                command.Parameters.Add("@projectname", SqlDbType.VarChar).Value = employeeDetail.Projectname;
+                command.Parameters.Add("@managermane", SqlDbType.VarChar).Value = employeeDetail.Managername;
+
+                connection.Open();
+                int result = command.ExecuteNonQuery();
+                connection.Close();
+
+                if (result == 1)
+                {
+                    viewMessage = "Record Entered Succesfully";
+                }
+                else
+                {
+                    viewMessage = "error";
+                }
+            }
+
+            return viewMessage;
+        }
+
+        public bool AssociateIdExists(string associateId)
+        {
+            bool isUserExists = false;
+            string connectionstring = ConfigurationManager.ConnectionStrings["Constring"].ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(connectionstring))
+            {
+                string userExistQuery = "Select Count(*) From dbo.empdetails Where associateid='" + associateId + "'";
+                using (SqlCommand command = new SqlCommand(userExistQuery, connection))
+                {
+                    connection.Open();
+                    isUserExists = Convert.ToBoolean(command.ExecuteScalar());
+                    connection.Close();
+                }
+            }
+
+            return isUserExists;
+        }
+        public List<EmployeeDetails> Home()
+        {
+            string connectionstring = ConfigurationManager.ConnectionStrings["Constring"].ConnectionString;
+            EmployeeDetails emplopyedetails=null;
+            List<EmployeeDetails> employee = new List<EmployeeDetails>();
+            using (SqlConnection connection = new SqlConnection(connectionstring))
+            {
+                SqlCommand cmd = new SqlCommand("select * from empdetails", connection);
+                connection.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    emplopyedetails = new EmployeeDetails();
+                    emplopyedetails.Associateid = rdr["associateId"].ToString();
+                    emplopyedetails.Role = rdr["role"].ToString();
+                    emplopyedetails.Associatename = rdr["associatename"].ToString();
+                    emplopyedetails.Projectname = rdr["projectname"].ToString();
+                    emplopyedetails.Managername = rdr["managername"].ToString();
+                    employee.Add(emplopyedetails);
+                }
+            }
+            return employee;
+        }
+
+    }
+}
---
+++ rlgemp/Services/EmployeeService.cs
@@ -0,0 +1,38 @@
+﻿using rlgemp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using rlgemp.Repository;
+
+namespace rlgemp.Services
+{
+    public class EmployeeService
+    {
+        EmployeeRepository employeeRepository;
+        public EmployeeService()
+        {
+            employeeRepository = new EmployeeRepository();
+        }
+        public string InsertEmployeeDetails(EmployeeDetails employeeDetail)
+        {
+            var result = string.Empty;
+            if (!employeeRepository.AssociateIdExists(employeeDetail.Associateid))
+            {
+                result = employeeRepository.InsertEmployeeDetails(employeeDetail);
+            }
+            else
+            {
+                result = "associateId Already Exist";
+            }
+
+            return result;
+        }
+
+        public List<EmployeeDetails> Home()
+        {
+            return employeeRepository.Home();
+        }
+    }
+
+}
