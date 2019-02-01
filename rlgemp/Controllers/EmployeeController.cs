fee5efb22db20e5d66e84a0161efe9d4fd4bbdd6
Cogassment <46102794+Cogassment@users.noreply.github.com>
2019-02-01 16:00:49 +0530

    Update EmployeeController.cs

--- rlgemp/Controllers/EmployeeController.cs
+++ rlgemp/Controllers/EmployeeController.cs
@@ -1,104 +1,55 @@
-﻿using rlgemp.Models;
+using rlgemp.Models;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Web.Mvc;
 using System.Configuration;
+using rlgemp.Services;
+using rlgemp.Repository;
 
 namespace rlgemp.Controllers
 {
     public class EmployeeController : Controller
     {
-        [HttpGet]
+        EmployeeService employeeservice;
+        public EmployeeController()
+        {
+            employeeservice = new EmployeeService();
+        }
+         [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
-        [HttpPost]
-        public ActionResult Index(EmployeeDetails employeedetail)
-        {
-            if (ModelState.IsValid == true)
-            {
-                if (AssociateIdExists(employeedetail.Associateid) != true)
-                {
-                    try
-                    {
-                        string connectionstring = ConfigurationManager.ConnectionStrings["Constring"].ConnectionString;
-                        SqlConnection connection = new SqlConnection(connectionstring);
-                        string query = "insert INTO empdetails VALUES(@id,@role,@associatename,@projectname,@managermane)";
-                        using (SqlCommand command = new SqlCommand(query, connection))
-                        {
-                            command.Parameters.Add("@id", SqlDbType.VarChar).Value = employeedetail.Associateid;
-                            command.Parameters.Add("@role", SqlDbType.VarChar).Value = employeedetail.Role;
-                            command.Parameters.Add("@associatename", SqlDbType.VarChar).Value = employeedetail.Associatename;
-                            command.Parameters.Add("@projectname", SqlDbType.VarChar).Value = employeedetail.Projectname;
-                            command.Parameters.Add("@managermane", SqlDbType.VarChar).Value = employeedetail.Managername;
-                            connection.Open();
-                            int result = command.ExecuteNonQuery();
-                            connection.Close();
-                            if (result == 1)
-                            {
-                                TempData["usermsg"] = "Record Entered Succesfully";
-                            }
-                            else
-                            {
-                                TempData["usermsg"] = "error";
-                            }
-                }
-                }
-                    catch (Exception ex)
-                    {
-                        TempData["usermsg"] = "error";
-                    }
-                }
-                else
-                {
-                    TempData["usermsg"] = "associateId Already Exist";
-                }
-           }
-            return RedirectToAction("Index");
-        }
 
         public ActionResult Home()
-        {
-            string connectionstring = ConfigurationManager.ConnectionStrings["Constring"].ConnectionString;
-            EmployeeDetails emplopyedetails;
-            List<EmployeeDetails> employee = new List<EmployeeDetails>();
-            using (SqlConnection connection = new SqlConnection(connectionstring))
-            {
-                SqlCommand cmd = new SqlCommand("select * from empdetails", connection);
-                connection.Open();
-                SqlDataReader rdr = cmd.ExecuteReader();
-                while (rdr.Read())
-                {
-                    emplopyedetails = new EmployeeDetails();
-                    emplopyedetails.Associateid = rdr["associateId"].ToString();
-                    emplopyedetails.Role = rdr["role"].ToString();
-                    emplopyedetails.Associatename = rdr["associatename"].ToString();
-                    emplopyedetails.Projectname = rdr["projectname"].ToString();
-                    emplopyedetails.Managername = rdr["managername"].ToString();
-                    employee.Add(emplopyedetails);
-                }
-            }
-            return Json(employee);
+        {         
+
+            var details = employeeservice.Home();
+
+            return Json(details);
         }
-        public bool AssociateIdExists(string associateid)
+
+        [HttpPost]
+        public ActionResult Index(EmployeeDetails employeeDetail)
         {
-            bool isUserExists = false;
-           
-            string connectionstring = ConfigurationManager.ConnectionStrings["Constring"].ConnectionString;
-            using (SqlConnection connection = new SqlConnection(connectionstring))
+            var EmployeeService = new EmployeeService();
+            try
             {
-                string userExistQuery = "Select Count(*) From dbo.empdetails Where associateid='" + associateid + "'";
-                using (SqlCommand command = new SqlCommand(userExistQuery, connection))
+                if (ModelState.IsValid == true)
                 {
-                    connection.Open();
-                    isUserExists = Convert.ToBoolean(command.ExecuteScalar());
-                    connection.Close();
+                    TempData["usermsg"] = EmployeeService.InsertEmployeeDetails(employeeDetail);
                 }
+
+                return RedirectToAction("Index");
             }
-            return isUserExists;
+            catch (Exception ex)
+            {
+                TempData["usermsg"] = ex.Message;
+                return RedirectToAction("Errorpage");
+            }
         }
+
     }
-}     
+}
